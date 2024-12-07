07953a9a015eeeab0cae6c016f695f9de28dc54c
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2024-12-08 01:09:05 +0200

    updated settings.py MEDIA_ROOT

--- Cosmetic_studio/settings.py
+++ Cosmetic_studio/settings.py
@@ -185,7 +185,7 @@ STATIC_ROOT = BASE_DIR / "static_files/"
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = BASE_DIR / "media_images/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
